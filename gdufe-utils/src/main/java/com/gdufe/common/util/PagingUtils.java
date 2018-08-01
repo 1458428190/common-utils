7fd724fc8efac92f49c389aed6512a20042a60d7
赖程锋 <laichengfeng@corp.netease.com>
2018-08-01 12:42:39 +0800

    优化代码

--- gdufe-utils/src/main/java/com/gdufe/common/util/PagingUtils.java
+++ gdufe-utils/src/main/java/com/gdufe/common/util/PagingUtils.java
@@ -11,29 +11,24 @@ public class PagingUtils {
      *  count : 数据总数
      *  maxNum : 一页显示多少
      */
-    public static long getTotalPage( long count , int maxNum){
-        if(count == 0){
-            count = 1;
-        }
-        long totalPage = count / maxNum + (count % maxNum >= 1 ? 1 : 0);
-        return totalPage;
+    public static long getTotalPage(long count, long maxNum){
+       if(count < 0 || maxNum <= 0) {
+           throw new IllegalArgumentException("count < 0 or maxNum <= 0");
+       }
+       return (count + maxNum - 1) / maxNum;
     }
 
     /**
-     *  根据页码,计算表中第几行 limit(start , maxNum);
+     *  根据页码,计算表中第几行 limit(start, maxNum);
      * @param page : 页码
      * @param maxNum : 一页显示多少
      * @return
      */
-    public static int getStart(int page , int maxNum){
-        int start = 0;
-        if(page == 1) {
-            start = 0;
-        }
-        else {
-            start = (page - 1) * maxNum;
-        }
-        return start;
+    public static long getStart(long page, long maxNum){
+       if(page <= 0 || maxNum <= 0) {
+           throw new IllegalArgumentException("page <=0 or maxNum <= 0");
+       }
+       return (page - 1) * maxNum;
     }
 
 }
